7d73c6ee68ff6638f3efb218153f9c70baf10ce9
Alan Rossi <s1097619@studenti.univpm.it>
2023-01-05 09:20:28 +0100

    Update query_alan.sql

--- progettorio_database_sql/query_alan.sql
+++ progettorio_database_sql/query_alan.sql
@@ -7,16 +7,20 @@
 
 -- 1. Inserimento nuovo collaboratore - Testato
 
+/*
 INSERT INTO Collaboratore(nome, cognome, email, cellulare)
 VALUES (...);
+*/
 
 INSERT INTO Collaboratore(nome, cognome, email, cellulare)
 VALUES ('Mario', 'Rossi', 'mario.rossi@gmail.com', '3401256789');
 
 -- 2. Inserimento nuovo sensore - Testato
 
+/*
 INSERT INTO Sensore(matricola, tipologia, anno_produzione, descrizione, versione_firwmare)
 VALUES (...);
+*/
 
 INSERT INTO Sensore(matricola, tipologia, anno_produzione, descrizione, versione_firwmare)
 VALUES ('1000000', 'idrometro', 2022, 'Asta graduata che serve a determinare la quota del pelo
@@ -26,25 +30,31 @@ VALUES ('1000000', 'idrometro', 2022, 'Asta graduata che serve a determinare la 
 -- NOTA: media_misurazioni a cosa fa riferimento visto che i sensori impiegati in una medesima
 --       sessione di campionamento possono essere di tipologie diverse? (ovviare con una nota a pié di pagina)
 
+/*
 INSERT INTO SessioneCampionamento(data_ora, durata, media_misurazioni, id_zona)
 VALUES (...);
+*/
 
 INSERT INTO SessioneCampionamento(data_ora, durata, media_misurazioni, id_zona)
 VALUES ('2022-12-15 10:15:13', 30.00, 7.9, 5);
 
 -- 4. Inserimento dei dati ottenuti dalla sessione di campionamento - Testato
 
+/*
 INSERT INTO Misurazione(valore_rilevato, timestamp, id_sessione, matricola_sensore)
 VALUES (...);
+*/
 
 INSERT INTO Misurazione(valore_rilevato, timestamp, id_sessione, matricola_sensore)
 VALUES (5.4, '2022-12-15 10:15:13', 4, 4353246674);
 
 -- 5. Inserimento delle soglie minime di pericolo - Testato
 
+/*
 UPDATE SessioneCampionamento
 SET val_min = <valore minimo>, val_max = <valore massimo>
 WHERE val_min IS NULL AND val_max IS NULL;
+*/
 
 UPDATE SessioneCampionamento
 SET val_min = 3.2, val_max = 8.7
@@ -77,9 +87,11 @@ FROM Post;
 
 -- 30. Visualizza utente della piattaforma proprietaria - Testato
 
+/*
 SELECT *
 FROM UtentePiattaforma
 WHERE id = <id utente piattaforma>;
+*/
 
 SELECT *
 FROM UtentePiattaforma
@@ -87,9 +99,11 @@ WHERE id = 3;
 
 -- 31. Visualizza diramazione allerta - Testato
 
+/*
 SELECT *
 FROM Allerta
 WHERE id = <id allerta>;
+*/
 
 SELECT *
 FROM Allerta
@@ -98,36 +112,59 @@ WHERE id = 2;
 -- QUERY DI MODIFICA
 -- ======================
 
--- 17. Modifica dati collaboratore
+-- 17. Modifica dati collaboratore - Testato
+
+/*
+UPDATE Collaboratore
+SET nome = <nome>, cognome = <cognome>, email = <indirizzo e-mail>, cellulare = <numero di cellulare>
+WHERE id = <id collaboratore>;
+*/
 
 UPDATE Collaboratore
-SET nome = <nome>, cognome = <cognome>, email = <indirizzo e-mail>, cellulare = <numero di cellulare>;
-WHERE id = <id collaboratore>
+SET nome = 'Mario', cognome = 'Rossi', email = 'mario.rossi@virgilio.it', cellulare = '3401256789'
+WHERE id = 6;
 
--- 18. Modifica dati sensore
+-- 18. Modifica dati sensore - Testato
 
+/*
 UPDATE Sensore
 SET tipologia = <tipologia>, anno_produzione = <anno di produzione>,
-    descrizione = <descrizione>, versione_firmware = <versione del firmware>;
-WHERE matricola = <matricola del sensore>
+    descrizione = <descrizione>, versione_firmware = <versione del firmware>
+WHERE matricola = <matricola del sensore>;
+*/
+
+UPDATE Sensore
+SET tipologia = 'idrometro', anno_produzione = 2022,
+    descrizione = 'Asta graduata che serve a determinare la quota del pelo liquido di un corso d''acqua, rispetto a una quota di riferimento',
+    versione_firwmare = 1.2
+WHERE matricola = 1000000;
 
 -- 19. Modifica utente della piattaforma proprietaria
 
+/*
 UPDATE UtentePiattaforma
 SET url = <url del profilo utente>, nome = <nome>, cognome = <cognome>,
     nickname = <nickname utente>, data_nascita = <data di nascita>,
     citta = <luogo di nascita>, password = <password>,
     email = <indirizzo e-mail>, cellulare = <numero di cellulare>
 WHERE id = <id utente piattaforma>;
+*/
 
--- 20. Modifica dell'indice di affidabilità dell'utente
+-- 20. Modifica dell'indice di affidabilità dell'utente (da escludere utente piattaforma o integrare la query) - Testato
 
+/*
 UPDATE Utente
 SET grado_attendibilità = <nuovo grado di attendibilità>
 WHERE id = <id utente>;
+*/
+
+UPDATE Utente
+SET grado_attendibilita = 9.5
+WHERE id = 1;
 
 -- 21. Modifica dell'indice di attendibilià del contenuto testuale/multimediale
 
+/*
 UPDATE Post
 SET grado_attendibilita = <nuovo grado di attendibilità>
 WHERE id = <id post>;
@@ -135,3 +172,4 @@ WHERE id = <id post>;
 UPDATE FileMultimediale
 SET grado_attendibilita = <nuovo grado di attendibilità>
 WHERE id = <id file multimediale>;
+*/
